46d2f4f818c4bf728e66752f435a70f9af8d56c1
Steven Petryk <me@stevenpetryk.com>
2017-08-21 11:06:56 -0400

    Rename streams

--- src/TranscoderStream.js
+++ src/TranscoderStream.js
@@ -1,13 +1,13 @@
 import { PassThrough } from 'stream'
 
-import wavStream from './WAVStream'
-import s3UploadStream from './S3UploadStream'
-import transcodeStream from './TranscodeStream'
+import wavStream from './wavStream'
+import s3UploadStream from './s3UploadStream'
+import ffmpegStream from './ffmpegStream'
 
 export default function () {
   const inputStream = new PassThrough()
 
-  transcodeStream(inputStream.pipe(wavStream))
+  ffmpegStream(inputStream.pipe(wavStream))
     .pipe(s3UploadStream('streams/133/transcoded.mp3'))
 
   return inputStream
